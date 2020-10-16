17ea4f4899b1fbbe8a2bf5765322918cdc6c794c
Vinod Kurpad <vikurpad@microsoft.com>
2020-10-16 12:12:17 -0700

    Updates

---
+++ precache-manifest.2e257607440bd183d1d0b3e75370a236.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389e6d23abc62ac94245da6758c2f2e0",
+    "url": "/azure-power-skills/index.html"
+  },
+  {
+    "revision": "15e965392218ac0e659d",
+    "url": "/azure-power-skills/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "068c60a2be7b2395ce90",
+    "url": "/azure-power-skills/static/js/2.838d10ac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/azure-power-skills/static/js/2.838d10ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e965392218ac0e659d",
+    "url": "/azure-power-skills/static/js/main.69d287eb.chunk.js"
+  },
+  {
+    "revision": "8fa3e6eebe9d5a901358",
+    "url": "/azure-power-skills/static/js/runtime-main.fddf509e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/azure-power-skills/static/media/logo.5d5d9eef.svg"
+  }
+]);
